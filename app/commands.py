53cb732048e4c903d759be334cde0b879c6ab628
korolevevev <64105968+korolevevev@users.noreply.github.com>
2020-05-27 13:21:40 +0300

    Update commands.py

--- app/commands.py
+++ app/commands.py
@@ -1,7 +1,7 @@
 import click
 from flask.cli import with_appcontext
 
-from app import db
+from .__init__ import db
 from app.models import User, Post, PostLike
 
 @click.command(name='create_tables')
